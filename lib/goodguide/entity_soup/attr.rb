0d42564064ded2e93c07aee7afc569ce121f4bcb
Ryan Aipperspach <ryan@goodguide.com>
2013-01-15 13:52:59 -0800

    Allow lookup of enum labels.

--- lib/goodguide/entity_soup/attr.rb
+++ lib/goodguide/entity_soup/attr.rb
@@ -2,14 +2,30 @@ require 'hashie/mash'
 module GoodGuide::EntitySoup
   class Attr
     include Resource
-    
+
     attributes :type, :name, :options, :entity_type, :catalog_id, :schema
-    
+
     default_view include: ['schema']
     view :bare, inherits: nil, schema: nil
 
     def self.types
       connection.get('types')['attr_types'].collect { |t| Hashie::Mash.new(t) }
+    end
+
+    def enum
+      enum = {}
+      if options[:enum] and options[:enum_titles]
+        options[:enum].zip(options[:enum_titles]).each do |(key, value)|
+          enum[key] = value
+        end
+      end
+
+      enum
+    end
+
+    def label_for(value)
+      puts "Looking up #{value} in #{enum.to_yaml}"
+      enum[value] || value
     end
   end
 end
