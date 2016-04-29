8ccce2bec0be84c3a1dae9e786e81a7ec7c39d90
Nathan Amick <github@nathanamick.com>
2016-04-29 09:21:10 +0000

    Add some dotfiles and env.local.sh example

---
+++ env.local.example.sh
@@ -0,0 +1,6 @@
+# Copy this file to `env.local.sh` and it will be sourced by
+# direnv in the .envrc file
+
+export PURVIEW_URL="localhost:3000"
+export PURVIEW_EMAIL="admin@goodguide.com"
+export PURVIEW_PASSWORD="password"
