a2fede2f365476ad0c0ed8133aa80b027954c2e6
Nathan Amick <github@nathanamick.com>
2016-05-31 10:22:36 -0500

    Improve the catalog's api
    - Move class variables to the top of the file in accordance to the style guide.
    
    - Grabbing a field by its name is the most common way to look up a specific field, just send a message `field` with its name.
    
    - Raise an error when a field is not found rather than failing.
    
    - Add bang versions of `fields` and `entities` to hit the api again rather than using the cached set.
    
    - Use the full module/class name rather than described_class

--- lib/purview_api/catalog.rb
+++ lib/purview_api/catalog.rb
@@ -1,4 +1,7 @@
 require "purview_api/resource"
+require "purview_api/entity"
+require "purview_api/field"
+require "purview_api/errors"
 
 module PurviewApi
   class Catalog
@@ -6,24 +9,47 @@ module PurviewApi
 
     define_attribute_methods(:description, :name, :entity_type, :is_reference, :account_id, :slug, :referenced_catalog_id)
 
+    def self.find_by_name(name, opts = {})
+      find_all(opts).find{|c| c.name == name.to_s}
+    end
+
+    def self.find_by_slug(slug, opts = {})
+      find_all(opts).find{|c| c.slug == slug.to_s}
+    end
+
     def fields(params = {})
-      Field.find_all(params.merge(:catalog_id => self.id))
+      @fields ||= PurviewApi::Field.find_all(params.merge(:catalog_id => id))
+    end
+
+    def fields!(params = {})
+      @fields = nil
+      fields(params)
+    end
+
+    def field(name)
+      fields.detect { |f| f.name == name.to_s }.tap do |field|
+        unless field
+          raise ResourceNotFound,
+            "Could not find a field named: '#{name}'"
+        end
+      end
+    end
+
+    def field_id(name)
+      field(name.to_s).id
     end
 
     def entities(params = {})
-      Entity.find_all(params.merge(:catalog_id => self.id))
+      @entities ||= PurviewApi::Entity.find_all(params.merge(:catalog_id => self.id))
+    end
+
+    def entities!(params = {})
+      @entities = nil
+      entities(params)
     end
 
     def referenced_catalog
       @referenced_catalog ||= Catalog.find(referenced_catalog_id) if referenced_catalog_id
-    end
-
-    def self.find_by_name(name, opts = {})
-      Catalog.find_all(opts).find{|c| c.name == name.to_s}
-    end
-
-    def self.find_by_slug(slug, opts = {})
-      Catalog.find_all(opts).find{|c| c.slug == slug.to_s}
     end
   end
 end
--- spec/lib/purview_api/catalog_spec.rb
+++ spec/lib/purview_api/catalog_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
-require "purview_api/catalog"
+require 'support/temporary_names'
+
+require 'purview_api/catalog'
+require 'purview_api/account'
 
 describe PurviewApi::Catalog, :vcr do
   before { PurviewApi.authenticate! }
 
-  describe '.find_all', :vcr do
+  let(:first_catalog) { PurviewApi::Catalog.find_all.first }
+
+  describe '.find_all' do
     it 'finds all the catalogs' do
-      catalogs = described_class.find_all
+      catalogs = PurviewApi::Catalog.find_all
 
       expect(catalogs.length).to be > 1
       expect(catalogs.first).to be_a_kind_of(PurviewApi::Catalog)
@@ -15,8 +20,7 @@ describe PurviewApi::Catalog, :vcr do
 
   describe '.find' do
     it 'finds the first catalog' do
-      first_catalog = described_class.find_all.first
-      catalog = described_class.find(first_catalog.id)
+      catalog = PurviewApi::Catalog.find(first_catalog.id)
 
       expect(catalog.name).to eq(first_catalog.name)
     end
@@ -33,7 +37,7 @@ describe PurviewApi::Catalog, :vcr do
       end
 
       it 'returns the errors' do
-        catalog = described_class.new
+        catalog = PurviewApi::Catalog.new
         expect(catalog.save).to be false
         expect(catalog.errors.messages).to eq(expected_messages)
       end
@@ -43,16 +47,78 @@ describe PurviewApi::Catalog, :vcr do
       let(:full_messages) { ['Name ["has already been taken"]'] }
 
       it 'returns "has already been taken" error' do
-        existing_catalog = described_class.find_all.first
-        catalog = described_class.new(
-          name: existing_catalog.name,
-          account_id: existing_catalog.account_id,
-          entity_type: existing_catalog.entity_type
+        catalog = PurviewApi::Catalog.new(
+          name: first_catalog.name,
+          account_id: first_catalog.account_id,
+          entity_type: first_catalog.entity_type
         )
 
         expect(catalog.save).to be(false)
         expect(catalog.errors.full_messages).to match(full_messages)
       end
+    end
+  end
+
+  describe '.field' do
+    before do
+      PurviewApi::Field.new(name: temporary_name('color'),
+                            catalog_id: first_catalog.id,
+                            type: 'StringField',
+                            position: 2).tap { |f| f.save! }
+    end
+
+    let(:color) { temporary_name(:color) }
+
+    it 'returns the field with the provided name' do
+      expect(first_catalog.field(color).name).to eq(temporary_name('color'))
+    end
+
+    it 'returns the field with the provided name as a symbol' do
+      expect(first_catalog.field(color.to_sym).name).to eq(temporary_name(:color))
+    end
+
+    it 'errors if there is no field by that name' do
+      expect{
+        first_catalog.field('not-a-field')
+      }.to raise_error(PurviewApi::Resource::ResourceNotFound)
+    end
+
+    after { clear_all_temporary_entities(first_catalog.fields) }
+  end
+
+  describe '.entities' do
+    it 'returns all entities' do
+      clear_all_temporary_entities(first_catalog.entities)
+      entity = PurviewApi::Entity.new(
+        catalog_id: first_catalog.id,
+        value_bindings: {
+          first_catalog.field('name').id => temporary_name('peanut butter')
+        }
+      )
+      entity.save!
+      wait_for_solr_if_vcr_is_recording
+      found_entity = first_catalog.entities!.detect { |e| e.id == entity.id }
+
+      expect(found_entity).to be_truthy
+
+      clear_all_temporary_entities(first_catalog.entities)
+    end
+  end
+
+  describe '.fields' do
+    it 'returns all fields' do
+      clear_all_temporary_entities(first_catalog.fields)
+      original_count = first_catalog.fields.size
+
+      field = PurviewApi::Field.new(name: temporary_name('color'),
+                                    catalog_id: first_catalog.id,
+                                    type: 'StringField',
+                                    position: 2)
+      field.save!
+
+      expect(first_catalog.fields!.size).to be > original_count
+
+      clear_all_temporary_entities(first_catalog.fields)
     end
   end
 end
